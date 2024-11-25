dd5f52fc5599e659421955efcdeb6ae62ec9d02e
Vishnu R <vishnuuragu@gmail.com>
2024-11-25 09:51:37 +0000

    Added UUID for safety purpose

--- sonali/custom_functions/Serial_No.py
+++ sonali/custom_functions/Serial_No.py
@@ -2,7 +2,13 @@ import frappe
 import qrcode
 from frappe.utils import get_site_path
 from frappe.utils.file_manager import save_file
+import random
+import string
 
+
+def generate_id(length=12):
+    char_set = string.ascii_letters + string.digits
+    return ''.join(random.SystemRandom().choice(char_set) for _ in range(length))
 
 @frappe.whitelist()
 def generate_qr_for_missing_serials():
@@ -20,5 +26,6 @@ def generate_qr_code(serial_no_doc):
     qr_code_path = get_site_path("public", "files", f"qr_{serial_no_doc.serial_no}.png")  # Path to save image
     qr_image.save(qr_code_path)  # Save the image
     serial_no_doc.custom_qr_image_link = f"/files/qr_{serial_no_doc.serial_no}.png"  # Link to the saved QR code
+    serial_no_doc.custom_uuid = generate_id()  # Generate a random UUID
     serial_no_doc.save()  # Save the Serial No record with the new qr_code_link
     print(f"QR code saved at {qr_code_path}")
