2c45d511f754d9ab31c78d0ebcd907b3d3692d4c
Vishnu R <vishnuuragu@gmail.com>
2025-01-02 10:41:25 +0000

    feat: Add QR code generation and new field for QR image in Bulk Coupon

--- sonali/custom_functions/Bulk_coupon.py
+++ sonali/custom_functions/Bulk_coupon.py
@@ -1,5 +1,7 @@
 import frappe
 import uuid
+import qrcode
+from frappe.utils import get_site_path
 
 @frappe.whitelist()
 def create_bulk_data(number_of_coupons, coupon_amount):
@@ -11,6 +13,13 @@ def create_bulk_data(number_of_coupons, coupon_amount):
             doc.coupon_price = coupon_amount  # Replace 'additional_field' with your actual fieldname
             uuid_v4 = uuid.uuid4()
             doc.uuid = str(uuid_v4)
+            qr_data = doc.uuid;  # Generate QR code data
+            qr_image = qrcode.make(qr_data)
+            qr_code_path = get_site_path("public", "files", f"{doc.uuid}.png")
+            qr_image.save(qr_code_path)
+            print(f"Generating QR code for Serial No: {doc.uuid}")
+            doc.qr_image = f"/files/{doc.uuid}.png"
+            print(doc.qr_image)
             doc.insert()
 
         return f"{number_of_coupons} records created successfully!"
